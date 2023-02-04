dfd85b20e7c0465b115a94e6c8d051a9a7a7c490
vankanna <passvankanna@gmail.com>
2023-02-04 16:29:03 -0600

    homejs

---
+++ main.js
@@ -0,0 +1,37 @@
+'use strict';
+
+const navbar = document.querySelector('#navbar');
+const navbarHeight = navbar.getBoundingClientRect().height;
+
+document.addEventListener('scroll', () =>{
+  console.log(window.scrollY);
+  console.log(`navbarHeight: ${navbarHeight}`);
+  if(window.scrollY > navbarHeight) {
+    navbar.classList.add('navbar--dark');
+  } else {
+    navbar.classList.remove('navbar--dark');
+  }
+});
+
+//handle scrolling when tapping on the navbar menu
+const navbarMenu = document.querySelector('.navbar__menu');
+navbarMenu.addEventListener('click', (event) =>{
+  const target = event.target;
+  const link = target.dataset.link;
+  if (link == null) {
+    return;
+  }
+  console.log(event.target.dataset.link);
+  scrollIntoView(link);
+});
+
+//Handle click on "contact me" button on home
+const homeContactBtn = document.querySelector('.home__contact');
+homeContactBtn.addEventListener('click', ()=>{
+  scrollIntoView('#contact');
+})
+
+function scrollIntoView(selector) {
+  const scrollTo = document.querySelector(selector);
+  scrollTo.scrollIntoView({behavior: "smooth"});
+}
